3cf1bb3eedaff9c022a4f560dd0533551c98500f
MaksimKisliak <makskislyak@gmail.com>
2023-03-13 16:51:36 +0100

    Add dosctings and comments

--- app/app.py
+++ app/app.py
@@ -53,6 +53,16 @@ class Event(HashModel):
 
 @app.get('/deliveries/{pk}/status')
 async def get_state(pk: str):
+    """
+    Retrieve the current state of a delivery by its primary key.
+
+    Parameters:
+        - pk (str): The primary key of the delivery.
+
+    Returns:
+        - A dictionary containing the current state of the delivery.
+
+    """
     state = redis.get(f'delivery:{pk}')
 
     if state is not None:
@@ -64,6 +74,16 @@ async def get_state(pk: str):
 
 
 def build_state(pk: str):
+    """
+    Build the current state of a delivery by its primary key.
+
+    Parameters:
+        - pk (str): The primary key of the delivery.
+
+    Returns:
+        - A dictionary containing the current state of the delivery.
+
+    """
     pks = Event.all_pks()
     all_events = [Event.get(pk) for pk in pks]
     events = [event for event in all_events if event.delivery_id == pk]
@@ -77,20 +97,45 @@ def build_state(pk: str):
 
 @app.post('/deliveries/create')
 async def create(request: Request):
+    """
+    Create a new delivery with the specified budget and notes.
+
+    Parameters:
+        - request (Request): The HTTP request containing the JSON payload with the budget and notes.
+
+    Returns:
+        - A dictionary containing the initial state of the new delivery.
+
+    """
     body = await request.json()
+    # Create a redis Hash object
     delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+    # Create a redis Hash object
     event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
     state = CONSUMERS[event.type]({}, event)
+    # Create a redis string
     redis.set(f'delivery:{delivery.pk}', json.dumps(state))
     return state
 
 
 @app.post('/event')
 async def dispatch(request: Request):
+    """
+    Dispatch an event to update the state of a delivery.
+
+    Parameters:
+        - request (Request): The HTTP request containing the JSON payload with the delivery ID, type of event,
+        and event data.
+
+    Returns:
+        - A dictionary containing the updated state of the delivery.
+
+    """
     body = await request.json()
     delivery_id = body['delivery_id']
     state = await get_state(delivery_id)
     event = Event(delivery_id=delivery_id, type=body['type'], data=json.dumps(body['data'])).save()
     new_state = CONSUMERS[event.type](state, event)
+    # Update the redis string with the new values
     redis.set(f'delivery:{delivery_id}', json.dumps(new_state))
     return new_state
--- app/consumers.py
+++ app/consumers.py
@@ -3,6 +3,17 @@ from fastapi import HTTPException
 
 
 def create_delivery(state, event):
+    """
+    Create a new delivery and initialize its state with the specified budget and notes.
+
+    Parameters:
+        - state (dict): The current state of the delivery.
+        - event (Event): The event that triggered the creation of the delivery.
+
+    Returns:
+        - A dictionary containing the initial state of the new delivery.
+
+    """
     data = json.loads(event.data)
     return {
         "id": event.delivery_id,
@@ -13,6 +24,20 @@ def create_delivery(state, event):
 
 
 def start_delivery(state, event):
+    """
+    Start a delivery and update its state to 'active'.
+
+    Parameters:
+        - state (dict): The current state of the delivery.
+        - event (Event): The event that triggered the start of the delivery.
+
+    Returns:
+        - A dictionary containing the updated state of the delivery.
+
+    Raises:
+        - HTTPException: If the delivery is already started.
+
+    """
     if state['status'] != 'ready':
         raise HTTPException(status_code=400, detail="Delivery already started")
 
@@ -22,6 +47,20 @@ def start_delivery(state, event):
 
 
 def pickup_products(state, event):
+    """
+    Update the state of a delivery to indicate that products have been picked up.
+
+    Parameters:
+        - state (dict): The current state of the delivery.
+        - event (Event): The event that triggered the update.
+
+    Returns:
+        - A dictionary containing the updated state of the delivery.
+
+    Raises:
+        - HTTPException: If the delivery is already completed or there is not enough budget to complete the pickup.
+
+    """
     data = json.loads(event.data)
     new_budget = state["budget"] - int(data['purchase_price']) * int(data['quantity'])
 
@@ -40,6 +79,20 @@ def pickup_products(state, event):
 
 
 def deliver_products(state, event):
+    """
+    Update the state of a delivery to indicate that products have been delivered.
+
+    Parameters:
+        - state (dict): The current state of the delivery.
+        - event (Event): The event that triggered the update.
+
+    Returns:
+        - A dictionary containing the updated state of the delivery.
+
+    Raises:
+        - HTTPException: If the delivery is already completed or there is not enough quantity to complete the delivery.
+
+    """
     data = json.loads(event.data)
     new_quantity = state["quantity"] - int(data['quantity'])
 
@@ -60,6 +113,20 @@ def deliver_products(state, event):
 
 
 def increase_budget(state, event):
+    """
+    Increase the budget of a delivery.
+
+    Parameters:
+        - state (dict): The current state of the delivery.
+        - event (Event): The event that triggered the budget increase.
+
+    Returns:
+        - A dictionary containing the updated state of the delivery.
+
+    Raises:
+        - HTTPException: If the delivery is not active.
+
+    """
     if state["status"] != "active":
         raise HTTPException(status_code=400, detail="delivery has either not started or has already been completed")
     data = json.loads(event.data)
